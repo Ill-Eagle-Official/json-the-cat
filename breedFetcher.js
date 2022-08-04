1a18b0b325ee625fe6eb13824154eeff71c073d3
Alexander Jackson <alexx.j.jackson@gmail.com>
2022-08-04 22:31:57 +0000

    got the breed fetcher working

---
+++ breedFetcher.js
@@ -0,0 +1,17 @@
+const request = require('request');
+const input = process.argv.slice(2);
+const breedFetchApi = `https://api.thecatapi.com/v1/breeds/search?q=${input}`;
+
+
+request(breedFetchApi, (error, response, body) => {
+  
+  const data = JSON.parse(body);
+  if (data.length === 0) {
+    console.log('Breed not valid!');
+    return;
+    }
+  if (!breedFetchApi) {
+    console.log(error);
+  }
+  console.log(data[0].description);
+  });
